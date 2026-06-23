d45797e3af6e46438eff5255977dd6638189b892
Shallies <81499972+shallies@users.noreply.github.com>
2026-06-24 06:59:09 +0800

    Enhance vlmcsd start function with checks and logging
    Added checks to ensure vlmcsd starts only if not already running. Included logging for service start and stop events.

--- trunk/user/vlmcsd/vlmcsd.sh
+++ trunk/user/vlmcsd/vlmcsd.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
 func_start(){
-	vlmcsd
-	logger -st "vlmcsd" "start"
+check_vlmcsd=`ps|grep "vlmcsd[ ]"`
+if [ -z "$check_vlmcsd" ] ; then
+	count=60
+	while [ $count -gt 0 ]; do
+		if [ $(nvram get ntp_ready) = 1 ] ; then
+			break
+		fi
+		count=$((count-1))
+		sleep 5
+	done
+
+	vlmcsd -l /tmp/kms.log
+	logger -t "vlmcsd" " 服务已启动"
+else
+	logger -t "vlmcsd" " 服务正在运行"
+fi
 }
 
 func_stop(){
 	killall -q vlmcsd
+	logger -t "vlmcsd" " 服务已停止"
 }
 
 case "$1" in
